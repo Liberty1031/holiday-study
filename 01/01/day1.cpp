c2bba3ecac7ff0f57e129ecd6d4a158b7c1136d0
Liberty1031 <2681035285@qq.com>
2026-02-03 20:29:06 +0800

    change and stay

--- 01/01/day1.cpp
+++ 01/01/day1.cpp
@@ -1,5 +1,11 @@
 #include<stdio.h>
 int main(){
-	printf("i love you!");
+//	printf("i love you!");
+//2026.2.3
+int num1 = 0;
+int num2 = 0;
+scanf_s("%d %d",&num1,&num2);
+int sum = num1 + num2;
+printf("%d\n",sum);
 	return 0;
 }
