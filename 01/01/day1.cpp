6c7226650433658bee59deafff2a0262e1dcce92
Liberty1031 <2681035285@qq.com>
2026-02-05 00:04:37 +0800

    unchange

--- 01/01/day1.cpp
+++ 01/01/day1.cpp
@@ -1,6 +1,6 @@
 #include<stdio.h>
-int a = 10;
 int main(){
+	#define a 100
 //	printf("i love you!");
 //2026.2.3
 //int num1 = 0;
@@ -9,10 +9,7 @@ int main(){
 //int sum = num1 + num2;
 //printf("%d\n",sum);
 //2026.2.4//
-	{
-		int a = 10;
-		printf("%d", a);
-	}
-	printf("%d", a);
+	int arr[a] = { 0 };
+	/*printf("%d",a);*/
 	return 0;
 }
